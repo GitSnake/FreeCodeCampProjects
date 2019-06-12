fa32bb70b5c75d98509e3cf5b03afa610ea73528
gitSnake <GitSnake@users.noreply.github.com>
2019-06-11 23:07:06 -0400

    Updated results string

--- 02-JS Algorithms & Data Structures Certification/01-Intro to JS/01-Word Blanks Mad Lib/script.js
+++ 02-JS Algorithms & Data Structures Certification/01-Intro to JS/01-Word Blanks Mad Lib/script.js
@@ -1,6 +1,6 @@
 function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
   // Your code below this line
-  var result = "";
+  var result = "It was really " + "hot" + ", and we " + "laughed" + " ourselves " + "silly.";
 
   // Your code above this line
   return result;
